c73517e07aa45d8d037ae3e015164a57fae5080f
WITCH-CRAFT MAGAZINE <157259386+witch-craftmagazine@users.noreply.github.com>
2026-04-12 23:18:44 -0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  root: '.',
-  base: '/noteblock-pianola/',   // 👈 REQUIRED for GitHub Pages
-  server: { port: 8000 },
-  assetsInclude: ['**/*.sf2', '**/*.mid', '**/*.glb'],
-})
